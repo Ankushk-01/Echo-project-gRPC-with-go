81ba9c4b24362b457cf939009311e8a2bff03bed
Ankushk-12 <ankush.zversal@gmail.com>
2023-09-28 00:20:29 +0530

    Added the project

---
+++ client/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	pb "echo/Echo/Echo"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultValue = "Hello"
+)
+
+var (
+	addr        = flag.String("addr", "localhost:7000", "Server address")
+	echoRequest = flag.String("echo", defaultValue, "echo message ")
+)
+
+func main() {
+	flag.Parse()
+	con, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Connection not Establish %v", err)
+	}else{
+		log.Println("Connection Established")
+	}
+	c := pb.NewEchoServiceClient(con)
+	defer con.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Echo(ctx, &pb.EchoRequest{EchoRequest: *echoRequest})
+
+	if err != nil {
+		log.Fatalf("Echo message return an error %v", err)
+	}
+
+	log.Println("Echo : ",r.GetEchoResponse())
+	
+
+}
---
+++ server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "echo/Echo/Echo"
+
+	"google.golang.org/grpc"
+)
+
+var defaultPort int = 7000
+var (
+	port = flag.Int("port", defaultPort, "Server Port")
+)
+
+type server struct {
+	pb.UnimplementedEchoServiceServer
+}
+
+func (s *server) Echo(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+	log.Printf("The message is : %v", in.GetEchoRequest())
+	var message string = in.GetEchoRequest()
+	return &pb.EchoResponse{EchoResponse: message}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterEchoServiceServer(s, &server{})
+
+	log.Printf("Server is listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
